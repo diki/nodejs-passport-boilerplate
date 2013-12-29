ec006336dd0b9bca2f5f29dcf482b07c24d86cf2
diki <musdikici@gmail.com>
2013-12-29 20:14:05 +0200

    refactoring

--- config/express.js
+++ config/express.js
@@ -5,40 +5,40 @@
 var express = require('express')
   , mongoStore = require('connect-mongo')(express)
   , flash = require('connect-flash')
-  , helpers = require('view-helpers')
+  , helpers = require('view-helpers');
 
 module.exports = function (app, config, passport) {
 
-  app.set('showStackError', true)
+  app.set('showStackError', true);
   // should be placed before express.static
   app.use(express.compress({
     filter: function (req, res) {
       return /json|text|javascript|css/.test(res.getHeader('Content-Type'));
     },
     level: 9
-  }))
-  app.use(express.favicon())
-  app.use(express.static(config.root + '/public'))
+  }));
+  app.use(express.favicon());
+  app.use(express.static(config.root + '/public'));
 
   // don't use logger for test env
   if (process.env.NODE_ENV !== 'test') {
-    app.use(express.logger('dev'))
+    app.use(express.logger('dev'))''
   }
 
   // set views path, template engine and default layout
-  app.set('views', config.root + '/app/views')
-  app.set('view engine', 'jade')
+  app.set('views', config.root + '/app/views');
+  app.set('view engine', 'jade');
 
   app.configure(function () {
     // dynamic helpers
-    app.use(helpers(config.app.name))
+    app.use(helpers(config.app.name));
 
     // cookieParser should be above session
-    app.use(express.cookieParser())
+    app.use(express.cookieParser());
 
     // bodyParser should be above methodOverride
-    app.use(express.bodyParser())
-    app.use(express.methodOverride())
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
 
     // express/mongo session storage
     app.use(express.session({
@@ -46,18 +46,18 @@ module.exports = function (app, config, passport) {
       store: new mongoStore({
         url: config.db,
         collection : 'sessions'
-      })
-    }))
+      });
+    }));
 
     // connect flash for flash messages
-    app.use(flash())
+    app.use(flash());
 
     // use passport session
-    app.use(passport.initialize())
-    app.use(passport.session())
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     // routes should be at the last
-    app.use(app.router)
+    app.use(app.router);
 
     // assume "not found" in the error msgs
     // is a 404. this is somewhat silly, but
@@ -72,12 +72,12 @@ module.exports = function (app, config, passport) {
 
       // error page
       res.status(500).render('500', { error: err.stack })
-    })
+    });
 
     // assume 404 since no middleware responded
     app.use(function(req, res, next){
       res.status(404).render('404', { url: req.originalUrl, error: 'Not found' })
-    })
+    });
 
   })
 }
